743ac7315449f01be1f4cb04ce104e8aba5dff96
Aman-Chandra05 <amanchandra081@gmail.com>
2020-11-21 15:49:14 +0530

    Cabfare

---
+++ myjavascript.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+	$(".errmsg").hide();
+	$("#result").hide();
+	$("#samelocation").hide();
+	$("#result button").click(function()
+		{
+			$("#result").hide();
+		});
+	$("#sub").click(function(ev)
+	{
+		ev.preventDefault();
+		var pickup=$("#pickup").val();
+		var drop=$("#drop").val();
+		var cab=$("#cab").val();
+		var luggage=Number($("#luggage").val());
+		if(pickup==null || drop==null || cab==null || !(Number.isInteger(luggage)) || luggage<0)
+		{
+			if(pickup==null)
+				$("#pickmsg").show();
+			if(drop==null)
+				$("#dropmsg").show();
+			if(cab==null)
+				$("#cabmsg").show();
+			if(!(Number.isInteger(luggage)) || luggage<0)
+				$("#luggmsg").show();
+		}
+		else
+		{
+			if(pickup==drop)
+			{
+				$("#samelocation").show();
+				$("#result").hide();
+			}
+			else
+			{
+				$.ajax({
+					url: 'fare.php',
+					type: 'post',
+					data: {pickup,drop,cab,luggage},
+					dataType: 'json',
+					success: function(result)
+					{
+						$(".errmsg").hide();
+						$("#result").show();
+						$("#pickuplocation").html($(".location[value="+result.pickup+"]").html());
+						$("#droplocation").html($(".location[value="+result.drop+"]").html());
+						$("#dist").html(result.dist);
+						$("#fare").html(result.price);
+					},
+					error: function()
+					{
+						alert("error");
+					}
+				});
+			}
+		}
+	});
+});
+function cabtype(ctype)
+{
+	$("#cabmsg").hide();
+	if(ctype=="micro")
+	{
+		$("#luggage").val('');
+		$("#luggage").attr('disabled','disabled');
+	}
+	else
+	{
+		$('#luggage').removeAttr('disabled');
+	}
+}
+function droperr()
+{
+	$("#dropmsg").hide();
+	$("#samelocation").hide();
+}
+function pickuperr()
+{
+	$("#pickmsg").hide();
+	$("#samelocation").hide();
+}
+function luggerr()
+{
+	$("#luggmsg").hide();
+}
